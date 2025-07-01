0a699954086e54391139feecffc8cb7ffb6efe7f
CodyBense <codybense@gmail.com>
2025-06-30 20:55:32 -0400

    starship, testing settings

--- config/starship.nix
+++ config/starship.nix
@@ -6,12 +6,16 @@
         settings = {
             add_newline = false;
             format = lib.concatStrings [
-                "$line_break"
-                "$packagee"
-                "$line_break"
-                "$character"
+                "$username$git_branch$character"
             ];
             scan_timeout = 10;
+            username = {
+                style_user = "white bold";
+                style_root = "black bold";
+                format = "[$user]($style) ";
+                disabled = false;
+                show_always = true;
+            };
             character = {
                 success_symbol = "->";
                 error_symbol = "->";
