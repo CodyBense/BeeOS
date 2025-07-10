aacad31de0f158bea621cc314f30bb25e37f864e
CodyBense <codybense@gmail.com>
2025-07-09 21:50:05 -0400

    script, start-hyprland updated

--- scripts/start-hyprland.nix
+++ scripts/start-hyprland.nix
@@ -3,17 +3,17 @@
 pkgs.writeShellScriptBin "start-hyprland" ''
     #!/usr/bin/env bash
 
-    pkillall -q swww
+    pkill -q swww
     sleep 0.5
     swww-deamon &
 
-    pkillall -q waybar
+    pkill -q waybar
     sleep 0.5
     waybar &
 
     nm-applet --indicator &
 
-    pkillall -q swaync
+    pkill -q swaync
     sleep 0.5
     swaync &
 ''
