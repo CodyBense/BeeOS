8092ddda726a7451226a905dd4dea1a28ff27761
CodyBense <codybense@gmail.com>
2025-07-11 19:15:51 -0400

    scripts, updated start-hyprland

--- scripts/start-hyprland.nix
+++ scripts/start-hyprland.nix
@@ -3,19 +3,19 @@
 pkgs.writeShellScriptBin "start-hyprland" ''
     #!/usr/bin/env bash
 
-    pkill -q swww
+    pkill -q swww-daemon
     sleep 0.5
     swww-deamon &
 
     swww img ~/BeeOS/wallpapers/cm-black-home.png
 
-    pkill -q waybar
+    pkill -q .waybar-wrapped
     sleep 0.5
     waybar &
 
     nm-applet --indicator &
 
-    pkill -q swaync
+    pkill -q .swaync-wrapped
     sleep 0.5
     swaync &
 ''
