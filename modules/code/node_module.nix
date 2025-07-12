56823f6733b39782226a6cf649121227cc92a78c
CodyBense <codybense@gmail.com>
2025-07-12 11:30:31 -0400

    code, added node_module

---
+++ modules/code/node_module.nix
@@ -0,0 +1,12 @@
+{ lib, config, pkgs, ... }:
+{
+    options = {
+        node_module.enable = lib.mkEnableOption "enables node_module";
+    };
+
+    config = lib.mkIf config.node_module.enable {
+        environment.systemPackages = with pkgs; [
+            nodejs_24
+        ];
+    };
+}
