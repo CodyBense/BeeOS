cf8c41cfabdacbfed1d5e333dfcde10a57c72810
CodyBense <codybense@gmail.com>
2025-07-12 11:30:46 -0400

    code_bundle, imported node_module

--- modules/code_bundle.nix
+++ modules/code_bundle.nix
@@ -4,11 +4,13 @@
         ./code/python_module.nix
         ./code/rust_module.nix
         ./code/go_module.nix
+        ./code/node_module.nix
     ];
 
     python_module.enable = lib.mkDefault true;
     rust_module.enable = lib.mkDefault true;
     go_module.enable = lib.mkDefault true;
+    node_module.enable = lib.mkDefault true;
 
     environment.systemPackages = with pkgs; [
         gcc
