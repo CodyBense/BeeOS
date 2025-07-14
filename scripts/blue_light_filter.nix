9a79d811a855ac97cb6b511c61306992ef54dabd
CodyBense <codybense@gmail.com>
2025-07-14 19:51:34 -0400

    scripts, updated blue_light_filter

--- scripts/blue_light_filter.nix
+++ scripts/blue_light_filter.nix
@@ -2,5 +2,20 @@
 
 pkgs.writeShellScriptBin "blue_light_filter" ''
     #!/usr/bin/env bash
-    hyprsunset -t 3000 &
+
+    pkill hyprsunset
+
+    sleep 0.5
+
+    temp=$1
+    gama=$2
+
+    if [[ -z "$gama" ]]; then
+        notify-send "Hyprsunset: Setting temp to $temp"
+        hyprsunset -t temp &
+    else
+        notify-send "Hyprsunset: Setting temp to $temp and gama to $gama"
+        hyprsunset -t temp -g $gama &
+    fi
+
 ''
