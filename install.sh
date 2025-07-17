30cc9265d727641fb48827e7ad9b607c195930b7
CodyBense <codybense@gmail.com>
2025-07-17 18:40:08 -0400

    install script, added set indicators and added creation of dirs

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Building from config"
 echo "Enter target host name"
 
 echo $hostName
@@ -8,13 +9,31 @@ sudo cp /etc/nixos/hardware-configuration.nix hosts/$hostName/
 
 sudo nixos-rebuild switch --flake .#$hostName
 
+echo "Linking nvim config"
+
 cd /home/$USER
-mkdir .config/nvim
 
 git clone https://github.com/CodyBense/dotfiles.git
+mkdir .config/nvim
 
 ln -s $HOME/dotfiles/nvim/.config/nvim $HOME/.config/nvim/
+
+echo "Adding flatpaks"
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 flatpak install flathub app.zen_browser.zen
+
+echo "Creating directories"
+
+if [ ! -d $HOME/workspaces/github.com/CodyBense ]; then
+    mkdir -p $HOME/workspaces/github.com/CodyBense
+fi
+
+if [ ! -d $HOME/workspaces/Projects ]; then
+    mkdir -p $HOME/workspaces/Projects
+fi
+
+echo "Completed setup"
+echo "Restarting"
+reboot
