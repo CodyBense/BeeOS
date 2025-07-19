6dbe4ad6b558ab34e02551cc27d675ffd7a6c8c9
CodyBense <codybense@gmail.com>
2025-07-19 09:20:52 -0400

    fonts, fixed typo

--- modules/nix/fonts.nix
+++ modules/nix/fonts.nix
@@ -4,7 +4,7 @@
     fonts.packages = with pkgs; [
         font-awesome
         jetbrains-mono
-        cascadia-mono
+        cascadia-code
         maple-mono.NF
         mononoki
         noto-fonts-cjk-sans
