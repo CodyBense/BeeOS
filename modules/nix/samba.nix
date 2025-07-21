451eff2b9ac6b01d767cf7c5805d9400f48d3b87
CodyBense <codybense@gmail.com>
2025-07-21 17:43:45 -0400

    samba, created a nix module for samba shares

---
+++ modules/nix/samba.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+{
+    fileSystems."/mnt/share" = {
+        device = "//192.168.1.243/data";
+        fsType = "cifs";
+        options = let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    };
+}
