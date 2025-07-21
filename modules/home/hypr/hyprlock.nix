b200a2546262d16b294dbdae3e25ed0588e115f9
CodyBense <codybense@gmail.com>
2025-07-21 19:52:44 -0400

    hyprlock, updated config

--- modules/home/hypr/hyprlock.nix
+++ modules/home/hypr/hyprlock.nix
@@ -3,38 +3,32 @@
 {
     programs.hyprlock = {
         enable = true;
-        background = {
-            path = "/home/codybense/BeeOS/wallpapers/cm-aesthetic.jpg";
-            blur_passes = 3;
-            blur_size = 1;
-        };
+        settings = {
+            background = [
+                {
+                    path = "/home/codybense/BeeOS/wallpapers/cm-aesthetic.jpg";
+                    blur_passes = 3;
+                    blur_size = 1;
+                }
+            ];
 
-        input-field = {
-            size = "200 30";
-            outline_thickness = 3;
-            dots_size = 0.3;
-            dots_center = true;
-            font_family = "JetBrains Mono Nerd Font Mono Semibold";
-            outer_color = "rgba(102, 92, 84, 1)";
-            inner_color = "rgba(60, 56, 54, 1)";
-            font_color = "rgba(189, 174, 147, 1)";
-            fade_on_empty = flase;
-            placehold_text = "Password...";
-            check_color = "rgb(215, 153, 33)";
-            fail_color = "rgb(204, 36, 29)";
-            fail_text = "<i><b>Wrong password</b></i>";
-        };
-
-        label = {
-            text = "$TIME";
-            text_align = center;
-            color = "rgba(189, 174, 147, 1)";
-            font_size = 45;
-            font_family = "JetBrains Mono Nerd Font Mono Semibold";
-
-            position = "0, 00";
-            halign = center;
-            valign = center;
+            input-field = [
+                {
+                    size = "200 30";
+                    outline_thickness = 3;
+                    dots_size = 0.3;
+                    dots_center = true;
+                    font_family = "JetBrains Mono Nerd Font Mono Semibold";
+                    outer_color = "rgba(102, 92, 84, 1)";
+                    inner_color = "rgba(60, 56, 54, 1)";
+                    font_color = "rgba(189, 174, 147, 1)";
+                    fade_on_empty = false;
+                    placehold_text = "Password...";
+                    check_color = "rgb(215, 153, 33)";
+                    fail_color = "rgb(204, 36, 29)";
+                    fail_text = "<i><b>Wrong password</b></i>";
+                }
+            ];
         };
     };
 }
