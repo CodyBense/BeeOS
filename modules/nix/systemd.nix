4c72610eb6e0957e0fbd47786ff70010d2fdae99
CodyBense <codybense@gmail.com>
2025-07-22 21:29:50 -0400

    systemd.timers updated

--- modules/nix/systemd.nix
+++ modules/nix/systemd.nix
@@ -3,7 +3,8 @@
 {
     systemd.services.blue_light_filter = {
         description = "Run blue light filter";
-        serviceConfig.ExecStart = "/etc/profiles/per-user/codybense/bin/blue_light_filter";
+        # serviceConfig.ExecStart = "/etc/profiles/per-user/codybense/bin/blue_light_filter";
+        serviceConfig.ExecStart = "/home/codybense/BeeOS/modules/home/scripts/blue_light_filter.sh";
         wantedBy = [ "timers.target" ];
     };
 
