19d4804d7c77b28e9b2d6af70c1d7703e468b718
CodyBense <codybense@gmail.com>
2025-08-01 22:21:07 -0400

    flake, added nvf

--- flake.nix
+++ flake.nix
@@ -1,69 +1,79 @@
 {
-	description = "BeeOS Flake";
+  description = "BeeOS Flake";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		stylix = {
-		    url = "github:danth/stylix";
-		    inputs.nixpkgs.follows = "nixpkgs";
-		};
-		sops-nix = {
-		    url = "github:Mic92/sops-nix";
-		    inputs.nixpkgs.follows = "nixpkgs";
-		};
-		quickshell = {
-		    url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-		    inputs.nixpkgs.follows = "nixpkgs";
-		};
-		nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-    outputs = {
-        self,
-        nixpkgs,
-        home-manager,
-        nix-flatpak,
-        ...
-    }@inputs:
-	let
-		system = "x86_64-linux";
-		host = "laptop";
-		username = "codybense";
-        overlays = [
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nix-flatpak,
+    nvf,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    host = "laptop";
+    username = "codybense";
+    overlays = [
+    ];
+  in {
+    nixosConfigurations = {
+      ${host} = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit system;
+          inherit inputs;
+          inherit username;
+          inherit host;
+        };
+        modules = [
+          ./hosts/${host}/configuration.nix
+          inputs.stylix.nixosModules.stylix
+          inputs.sops-nix.nixosModules.sops
+          nix-flatpak.nixosModules.nix-flatpak
+          home-manager.nixosModules.home-manager
+          nvf.nixosModules.default
+          {
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+              inherit username;
+              inherit host;
+            };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.${username} = ./hosts/${host}/home.nix;
+            # home-manager.lib.homeManagerConfiguration = {
+            #     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            #     modules = [
+            #         nvf.homeManagerModules.default
+            #     ];
+            # };
+          }
         ];
-	in
-	{
-		nixosConfigurations = {
-			${host} = nixpkgs.lib.nixosSystem {
-				specialArgs = {
-					inherit system;
-					inherit inputs;
-					inherit username;
-					inherit host;
-				};
-				modules = [
-					./hosts/${host}/configuration.nix
-					inputs.stylix.nixosModules.stylix
-					inputs.sops-nix.nixosModules.sops
-					nix-flatpak.nixosModules.nix-flatpak
-					home-manager.nixosModules.home-manager
-						{
-							home-manager.extraSpecialArgs = {
-								inherit inputs;
-								inherit username;
-								inherit host;
-							};
-							home-manager.useGlobalPkgs = true;
-							home-manager.useUserPackages = true;
-							home-manager.backupFileExtension = "backup";
-							home-manager.users.${username} = ./hosts/${host}/home.nix;
-						}
-				];
-			};
-		};
-	};
+      };
+    };
+  };
 }
