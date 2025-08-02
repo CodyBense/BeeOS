8a6ba943320c88a433acac3af4c2a6ca434559e7
CodyBense <codybense@gmail.com>
2025-08-01 23:41:25 -0400

    nvf, updated

--- modules/home/nvf.nix
+++ modules/home/nvf.nix
@@ -126,47 +126,48 @@
                 autocomplete.nvim-cmp.enable = true;
 
                 languages = {
-                    enableLSP = true;
                     enableTreesitter = true;
 
                     nix.enable = true;
+                    python.enable = true;
                 };
 
                 lsp = {
-                trouble = {
                     enable = true;
-                    mappings = {
-                        documentDiagnostics = "<leader>xx";
-                        locList = "<leader>xl";
-                        lspReferences = "<leader>lr";
-                        quickfix = "<leader>xq";
+                    trouble = {
+                        enable = true;
+                        mappings = {
+                            documentDiagnostics = "<leader>xx";
+                            locList = "<leader>xl";
+                            lspReferences = "<leader>lr";
+                            quickfix = "<leader>xq";
+                        };
                     };
                 };
-            };
-            navigation.harpoon = {
-                enable = true;
-                mappings = {
-                    markFile = "<leader>a";
-                    listMarks = "<C-e>";
-                    file1 = "<C-1>";
-                    file2 = "<C-2>";
-                    file3 = "<C-3>";
-                    file4 = "<C-4>";
+                navigation.harpoon = {
+                    enable = true;
+                    mappings = {
+                        markFile = "<leader>a";
+                        listMarks = "<C-e>";
+                        file1 = "<C-1>";
+                        file2 = "<C-2>";
+                        file3 = "<C-3>";
+                        file4 = "<C-4>";
+                    };
                 };
-            };
-            terminal.toggleterm.lazygit = {
-                enable = true;
-                package = null;
-                direction = "float";
-                mappings.open = "<leader>lg";
-            };
-            telescope = {
-                enable = true;
-                mappings = {
-                    findFiles = "<leader>pf";
-                    liveGrep = "<leader>pg";
+                terminal.toggleterm.lazygit = {
+                    enable = true;
+                    package = null;
+                    direction = "float";
+                    mappings.open = "<leader>lg";
                 };
-            };
+                telescope = {
+                    enable = true;
+                    mappings = {
+                        findFiles = "<leader>pf";
+                        liveGrep = "<leader>pg";
+                    };
+                };
             };
         };
     };
