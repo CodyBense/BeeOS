2f79eba5d10dc48ef1860c6707e19e7291704b3e
CodyBense <codybense@gmail.com>
2025-08-07 20:13:58 -0400

    install script, fixed some warnings from the lsp

--- install.sh
+++ install.sh
@@ -1,54 +1,54 @@
 #!/usr/bin/env bash
 
-if ! [ $(id -u) = 0 ]; then
+if ! [ "$(id -u)" = 0 ]; then
     echo "The script needs to be run as root" >&2
     exit 1
 fi
 
 echo "Building from config"
 
-read -p "Enter target host name, defaults to laptop if none entered: " hostName
+read -pr "Enter target host name, defaults to laptop if none entered: " hostName
 
 if [ -z "$hostName" ]; then
     hostname=laptop
 fi
 
-if [ ! -d $HOME/BeeOS/hosts/$hostName ]; then
-    mkdir -p $HOME/BeeOS/hosts/E+$hostName
-    sudo cp /etc/nixos/configuration.nix hosts/$hostName
-    sudo cp /etc/nixos/hardware-configuration.nix hosts/$hostName
+if [ ! -d "$HOME"/BeeOS/hosts/"$hostName" ]; then
+    mkdir -p "$HOME"/BeeOS/hosts/"$hostName"
+    sudo cp /etc/nixos/configuration.nix hosts/"$hostName"
+    sudo cp /etc/nixos/hardware-configuration.nix hosts/"$hostName"
 fi
 
-sudo cp /etc/nixos/hardware-configuration.nix hosts/$hostName/
+sudo cp /etc/nixos/hardware-configuration.nix hosts/"$hostName"/
 
-sudo nixos-rebuild switch --flake .#$hostName
+sudo nixos-rebuild switch --flake .#"$hostName"
 
 echo "Linking nvim config"
 
-cd /home/$USER
+cd /home/"$USER"
 
 git clone https://github.com/CodyBense/dotfiles.git
 mkdir .config/nvim
 
-ln -s $HOME/dotfiles/nvim/.config/nvim $HOME/.config/nvim/
+ln -s "$HOME"/dotfiles/nvim/.config/nvim "$HOME"/.config/nvim/
 
 echo "Creating directories"
 
-if [ ! -d $HOME/workspaces/github.com/CodyBense ]; then
-    mkdir -p $HOME/workspaces/github.com/CodyBense
+if [ ! -d "$HOME"/workspaces/github.com/CodyBense ]; then
+    mkdir -p "$HOME"/workspaces/github.com/CodyBense
 fi
 
-if [ ! -d $HOME/workspaces/projects ]; then
-    mkdir -p $HOME/workspaces/projects
+if [ ! -d "$HOME"/workspaces/projects ]; then
+    mkdir -p "$HOME"/workspaces/projects
 fi
 
 echo "Adding info for samba share"
 
-read -p "Enter username for vault samba share: " username
-read -p "Enter passwrod for vault samba share: " password
+read -pr "Enter username for vault samba share: " username
+read -pr "Enter passwrod for vault samba share: " password
 
 echo -e "username=${username}\npassword=${password}" > /etc/nixos/smb-secrets
 
 echo "Completed setup"
-echo "Restarting"
+echo "Restarting..."
 reboot
