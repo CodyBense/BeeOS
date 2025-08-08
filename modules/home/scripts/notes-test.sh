81eea1ed9c525800e8ba27aad6665f6266ab3657
CodyBense <codybense@gmail.com>
2025-08-08 11:32:04 -0400

    scripts, added shell script to test rofi-notes and added it as a shell script bin .nix

---
+++ modules/home/scripts/notes-test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+choice="$(printf "Pull\nPush" | rofi -dmenu)" || exit 0
+
+case $choice in 
+    Pull) rsync -vr cody@192.168.1.243:/data/Notes $HOME/Documents/Notes;;
+    Push) rsync -vr $HOME/Documents/Notes cody@192.168.1.243:/data;;
+esac
