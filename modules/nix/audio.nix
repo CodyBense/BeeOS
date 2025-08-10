e64626e12aba2ef0f4e62e440ea076fc5bf9581b
CodyBense <codybense@gmail.com>
2025-08-10 18:34:54 -0400

    modules/nix, added audio modules

---
+++ modules/nix/audio.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+    environment.systemPackages = with pkgs; [
+        alsa-firmware
+        alsa-utils
+        inxi
+        sof-firmware
+    ];
+
+    service.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+    };
+}
