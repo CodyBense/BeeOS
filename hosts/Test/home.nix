69b31acaaf10284f58ce3dd847bbafbf4d856327
CodyBense <codybense@gmail.com>
2025-08-17 19:18:50 -0400

    test host

---
+++ hosts/Test/home.nix
@@ -0,0 +1,9 @@
+{ pkgs, username, ... }:
+
+{
+    home.username = "${username}";
+    home.homeDirectory = "/home/${username}";
+    home.stateVersion = "25.11";
+
+    programs.home-manager.enable = true;
+}
