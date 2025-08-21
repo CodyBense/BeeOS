2a0c6154ba62820f23f200e42ab17d70515cfbbb
CodyBense <codybense@gmail.com>
2025-08-21 17:24:18 -0400

    home, changed to unstable

--- hosts/Test/home.nix
+++ hosts/Test/home.nix
@@ -3,7 +3,8 @@
 {
     home.username = "${username}";
     home.homeDirectory = "/home/${username}";
-    home.stateVersion = "25.11";
+    # home.stateVersion = "25.11";
+    home.stateVersion = "unstable";
 
     programs.home-manager.enable = true;
 }
