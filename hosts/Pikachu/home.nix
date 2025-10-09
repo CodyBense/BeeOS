a4c0fd17941cb836c40dd595950160300e24b60e
Cody Bense <codybense@gmail.com>
2025-10-09 17:19:30 -0400

    REMOVED: nvim config source

--- hosts/Pikachu/home.nix
+++ hosts/Pikachu/home.nix
@@ -4,6 +4,4 @@
     home.username = "codybense";
     home.homeDirectory = "/home/codybense";
     home.stateVersion = "25.05";
-
-    home.file.".config/nvim".source = ../../modules/dotfiles/nvim;
 }
