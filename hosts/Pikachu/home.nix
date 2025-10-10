8ade2385a5b4f1c1ce473b06694431110f507a08
CodyBense <codybense@gmail.com>
2025-10-10 13:08:10 -0400

    home: added file source for starship, bashrc, and bash profile

--- hosts/Pikachu/home.nix
+++ hosts/Pikachu/home.nix
@@ -4,4 +4,8 @@
     home.username = "codybense";
     home.homeDirectory = "/home/codybense";
     home.stateVersion = "25.05";
+
+    home.file.".config/starship.toml".source = ../../modules/dotfiles/starship.toml;
+    home.file.".bashrc".source = ../../modules/dotfiles/.bashrc;
+    home.file.".bash_profile".source = ../../modules/dotfiles/.bash_profile;
 }
